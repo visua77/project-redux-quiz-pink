0009aab65cbcb9dd103117144576e8bce422addd
Krasnal <cislowski.karol@hotmail.com>
2020-01-09 16:20:19 +0100

    images added

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -8,18 +8,24 @@ export const Answer = () => {
   const answer = useSelector(state => state.quiz.answers[questionIndex]);
 
   const dispatch = useDispatch();
+  const photoPath = `../img/${answer.questionId}.jpg`;
 
   return (
     <div>
       {answer.isCorrect && <h3>YEAH!! {answer.answer} is a GOOD answer!!</h3>}
       {!answer.isCorrect && (
         <h3>
-          No, {answer.answer} is wrong. Correct answer was    {answer.question.options[answer.question.correctAnswerIndex]}
+          No, {answer.answer} is wrong. Correct answer was{" "}
+          {answer.question.options[answer.question.correctAnswerIndex]}
         </h3>
       )}
+      <img src={photoPath} />
       <button
         type="button"
-        onClick={() => dispatch(quiz.actions.goToNextQuestion())}>NEXT</button>
+        onClick={() => dispatch(quiz.actions.goToNextQuestion())}
+      >
+        NEXT
+      </button>
     </div>
   );
 };
